f688029410d73316d41906b2f952192f9e0464d5
dy5299 <dykgit@gmail.com>
2020-04-24 00:29:23 +0900

    soft url links

--- django/imgdesc/urls.py
+++ django/imgdesc/urls.py
@@ -7,7 +7,7 @@ app_name = 'imgdesc'
 
 urlpatterns = [
     path('', views.IndexView.as_view()),
-    path('list/', views.ListView.as_view()),
+    path('list/', views.ListView.as_view(), name='list'),
     path('<int:pk>/<mode>/', views.BoardView.as_view(), name='board'),
 ]
 
