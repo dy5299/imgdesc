37b681fcc6c5bc7ea11d90672740eef440a79c9e
dy5299 <dykgit@gmail.com>
2020-04-24 19:35:26 +0900

    login formify

--- django/accounts/views.py
+++ django/accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404,HttpResponse,HttpResponseRedirect
 
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
@@ -20,19 +20,25 @@ class SignupView(View):
 
 
 
+
 class SigninView(View):
     def get(self, request):
         return render(request, "accounts/signin.html")
 
     def post(self, request):
         #Loging 처리
+
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
-#        if user == None :
-#            return redirect('signin')    #urls.py에서 지정한 name. NOT 경로명.
+
+        if user is None :
+            return redirect('accounts:signin')
+#            return HttpResponseRedirect('/')
+
         #로그인 성공한 경우
-        login(request,user)
+        if user is not None :
+            login(request, user)
         return redirect('index')
 
 class SignoutView(View):
